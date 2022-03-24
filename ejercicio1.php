ed157c36e388ea9f4147525723b7fea1cd08c55b
MJFrancoM <ma.josefrancom@gmail.com>
2022-03-24 10:51:02 +0100

    Ejercicio 1 Pares o Impares

---
+++ ejercicio1.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP: Funciones_ Ejercicio 1</title>
+</head>
+<body>
+    <?php 
+    
+        $miNumero = 34;
+
+        echo mensaje($miNumero);
+
+        function mensaje($miNumero){
+    
+            if($miNumero % 2 == 0){
+                $mensaje="El número es par";
+            }else {
+                $mensaje="El número no es par";
+            }
+            echo $mensaje;
+
+        }
+    ?>
+</body>
+</html>
